15c609a921c3823eac169ef1d4b33ba5aa26d152
Phiju <PhijuFile@phijus-air.lan>
2021-02-10 02:32:13 +0800

    hii

--- Js/main.js
+++ Js/main.js
@@ -9,3 +9,18 @@ addToDoButton.addEventListener("click", function () {
   toDoContainer.appendChild(paragraph);
 });
 
+function openNav() {
+  //opens side navbar by 70 percent
+  document.getElementById("mySidenav").style.width = "70%" 
+
+ //opens overlay display
+  document.getElementById('backdrop').style.display = "block" 
+}
+
+function closeNav() {
+  //closes side navbar totally
+  document.getElementById("mySidenav").style.width = "0"
+
+  //removes overlay display
+  document.getElementById('backdrop').style.display = "none"
+}
